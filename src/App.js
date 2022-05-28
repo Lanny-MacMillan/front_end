a7867da16173671e61b89d9afb0b8e52bb06484a
Lanny Mac <Lanny@MacBook-Pro.local>
2022-05-28 16:19:27 -0400

    front end functional locally, needs to be linked to heroku. light div arrangement for wireframe layout, added img but ideally would like to work it in with gifs and stuff

--- src/App.js
+++ src/App.js
@@ -5,49 +5,60 @@ import Post from './components/Post'
 
 
 function App() {
+
   const [body, setBody] = useState('')
-  const [tag, setTag] = useState('')
+  const [img, setImg] = useState('')
+  const [tags, setTags] = useState('')
   const [posts, setAllPosts] = useState([])
+
+  const APIBaseURL = 'http://localhost:3000/'
+  // URL below isnt correct its just a placeholder
+  // const APIBaseURL = 'https://stormy-temple-25752.herokuapp.com/'
 
   
   const handleNewBody = (event) => {
     setBody(event.target.value)
   }
+  const handleNewImg = (event) => {
+    setImg(event.target.value)
+  }
   const handleNewTag = (event) => {
-    setTag(event.target.value)
+    setTags(event.target.value)
   }
   const handleFormSubmit = (event) => {
     event.preventDefault();
     axios.post(
-      'http://localhost:3000/posts',
+      APIBaseURL + 'posts/',
       {
         body:body,
-        tag:tag,
+        img:img,
+        tags:tags,
       }//then request is so we dont have to reload page on submission
     ).then(()=>{
       axios
-          .get('http://localhost:3000/posts')
+          .get(APIBaseURL + 'posts/')
           .then((response)=>{
               setAllPosts(response.data)
           })
       })
   }
 
-  
+
   // ============ NEEDS API ADDRESS ===================================================
   const editPost = (event, postData) => {
     event.preventDefault()
       axios
         .put(
-          `http://localhost:3000/posts/${postData._id}`,
+          APIBaseURL + `posts/${postData._id}`,
           {
             body:body,
-            tag:tag,
+            img:img,
+            tags:tags,
           }
       )
       .then(() => {
         axios
-          .get('http://localhost:3000/posts')
+          .get(APIBaseURL + 'posts/')
           .then((response) => {
               setAllPosts(response.data)
           })
@@ -57,20 +68,19 @@ function App() {
   // ============ NEEDS API ADDRESS ===================================================
   const removePost = (postData)=>{
     axios
-        .delete(`http://localhost:3000/posts/${postData._id}`)
+        .delete(APIBaseURL + `posts/${postData._id}`)
         .then(()=>{
             axios
-                .get('http://localhost:3000/posts')
+                .get(APIBaseURL + 'posts/')
                 .then((response)=>{
                     setAllPosts(response.data)
                 })
         })
   }
 
-  // ============ NEEDS API ADDRESS ===================================================
   useEffect(() => {
     axios 
-        .get('http://localhost:3000/posts')
+        .get(APIBaseURL + 'posts/')
         .then((response) => {
           // console.log(response.data)
           setAllPosts(response.data)
@@ -79,26 +89,41 @@ function App() {
 
   return (
     <>
-    <h1>Bottle App</h1>
-    <section>
+    <div id='title'>Bottle App</div>
+
+    <div className='container'>
+    <div id='createDiv'>
       <h2>Message Here</h2>
         <form onSubmit={handleFormSubmit}>
           <input type='text' placeholder='Body' onChange={handleNewBody} required/><br/>
+          <input type='text' placeholder='Img' onChange={handleNewImg} required/><br/>
           <input type='text' placeholder='Tag' onChange={handleNewTag} required/><br/>
           <input type='submit' value='Create Post'/>
         </form><br/>
-    </section>
+    </div>
+    </div>
     <div className='container'>
     {posts.map((post)=> {
             return <Post 
             //    Props   =    Values
               removePost={removePost} 
               post={post} //this one confuses me but works
+              img={img}
+              tags={tags}
               editPost={editPost}
               handleNewBody={handleNewBody}
+              handleNewImg={handleNewImg}
               handleNewTag={handleNewTag}
               key={posts._id}/>
           })}
+    </div>
+    <div class='sidenav'>
+      <ul>
+        <li><a href='https://cloud.mongodb.com/v2/6290d9dd104b8b4c06555ef8#metrics/replicaSet/6290da14a1cd8c01b39440e9/explorer/test/posts/find'>Atlas DB</a></li>
+        <li><a href='https://github.com/Lanny-MacMillan/front_end'>Git Front</a></li>
+        <li><a href='https://github.com/Lanny-MacMillan/back_end'>Git Back</a></li>
+   
+      </ul>
     </div>
     </>
   );
--- src/components/Post.js
+++ src/components/Post.js
@@ -4,21 +4,24 @@ const Post = (props) => {
     //====================== POST TO DIV CARD =========================
 
         <div id='post-div'>
+            <h4>Tag: {props.post.tags}</h4>
             <h3>Body: {props.post.body}</h3>
-            <h4>Tag: {props.post.tag}</h4>
-
+            <img src={props.post.img} ></img>
+            {/* <h4>Tag: {props.post.tags.map(tags)} */}
+        
         {/* ===================== EDIT POST =========================*/}
 
-            <h4>Edit Post</h4>
+            {/* <h4>Edit Post</h4> */}
                 <form onSubmit={(event) => {props.editPost(event, props.post)}}>
-                    <input placeholder={props.post.body} onChange={props.handleNewBody} type="text" required/><br/>
-                    <input placeholder={props.post.tag} onChange={props.handleNewTag} type="text" required/><br/>
+                    <input placeholder={props.post.body} onChange={props.handleNewBody}  type="text" required/><br/>
+                    <input placeholder={props.post.img} onChange={props.handleNewImg} type="text" required/><br/>
+                    <input placeholder={props.post.tags} onChange={props.handleNewTag} type="text" required/><br/>
                     <input id='button' type="submit" value="Edit Message"/>
                 </form>
 
                 {/* ================ DELETE POST ==================== */}
 
-            <button onClick={(event) => {props.removePost(props.post)}}>Adopt</button>
+            <button onClick={(event) => {props.removePost(props.post)}}>Delete</button>
 
         </div>
     )
