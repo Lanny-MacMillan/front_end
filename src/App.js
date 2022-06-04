276fe3b1d079c64a3a5ed7fb5ad1f353b1baa68d
Lanny Mac <Lanny@MacBook-Pro.local>
2022-06-03 22:14:21 -0400

    working again

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Chat from './components/Chat';
 import io from "socket.io-client";
 import Fuse from 'fuse.js';
 import { Switch } from 'antd'
-//May have to connect this to back or front end heroku
 const socket = io.connect("http://localhost:3001");
 
 function App() {
@@ -42,7 +41,7 @@ function App() {
 
   const APIBaseURL = 'http://localhost:3000/'
   // URL below is to our backend...
-  // const APIBaseURL2 = 'https://stark-crag-15310-backend.herokuapp.com/'
+  // const APIBaseURL = 'https://stark-crag-15310-backend.herokuapp.com/'
 
   // ================================= SWITCH TOGGLE COMMENT ===============================
 
