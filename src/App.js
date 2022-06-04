44e18a6529c44e1ed04a514e02d226c87c2b6913
Lanny Mac <Lanny@MacBook-Pro.local>
2022-06-03 22:18:54 -0400

    hide feature broke on deploy but is fixed now

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import io from "socket.io-client";
 import Fuse from 'fuse.js';
 import { Switch } from 'antd'
 const socket = io.connect("http://localhost:3001");
-// const socket = io.connect("https://mighty-tor-36415-frontend.herokuapp.com/");
 
 function App() {
   const [name, setName] = useState('')
@@ -40,26 +39,22 @@ function App() {
 
   // ==================================== API BASE URLS ====================================
 
-  // const APIBaseURL = 'http://localhost:3000/'
+  const APIBaseURL = 'http://localhost:3000/'
   // URL below is to our backend...
-
   // const APIBaseURL = 'https://stark-crag-15310-backend.herokuapp.com/'
-
-  const APIBaseURL = 'https://stark-crag-15310-backend.herokuapp.com/'
-
 
   // ================================= SWITCH TOGGLE COMMENT ===============================
 
   const toggleComments = (postData, event) => {
     newShowComments ? setNewShowComments(false): setNewShowComments(true)
     axios
-    .put(`APIBaseURL/${postData._id}`, 
+    .put(`http://localhost:3000/posts/${postData._id}`, 
       {
       showComments: newShowComments
       })
     .then(()=>{
       axios
-          .get('APIBaseURL').then((response)=>{
+          .get('http://localhost:3000/posts').then((response)=>{
           setAllPosts(response.data)
     })
   })
@@ -70,13 +65,13 @@ function App() {
   const toggleEdit = (postData, event) => {
     newShowEdit ? setNewShowEdit(false): setNewShowEdit(true)
     axios
-    .put(`APIBaseURL/${postData._id}`, 
+    .put(`http://localhost:3000/posts/${postData._id}`, 
       {
       showEdit: newShowEdit
       })
     .then(()=>{
       axios
-          .get('APIBaseURL').then((response)=>{
+          .get('http://localhost:3000/posts').then((response)=>{
           setAllPosts(response.data)
     })
   })
@@ -87,13 +82,13 @@ function App() {
   const toggleDelete = (postData, event) => {
     newShowDelete ? setNewShowDelete(false): setNewShowDelete(true)
     axios
-    .put(`APIBaseURL/${postData._id}`, 
+    .put(`http://localhost:3000/posts/${postData._id}`, 
       {
       showDelete: newShowDelete
       })
     .then(()=>{
       axios
-          .get('APIBaseURL').then((response)=>{
+          .get('http://localhost:3000/posts').then((response)=>{
           setAllPosts(response.data)
     })
   })
@@ -152,7 +147,6 @@ function App() {
         showComments: false
       }//then request is so we dont have to reload page on submission
     ).then(()=>{
-      event.target.reset()
       axios
           .get(APIBaseURL + 'posts/')
           .then((response)=>{
@@ -172,7 +166,6 @@ function App() {
           }
       )
       .then(() => {
-        event.target.reset()
         axios
           .get(APIBaseURL + 'posts/')
           .then((response) => {
@@ -359,4 +352,5 @@ function App() {
 }
 
 export default App;
+
 
