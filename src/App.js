7a37733a9d2f91703d33022fa0b4e639ebdc2052
Lanny Mac <Lanny@MacBook-Pro.local>
2022-06-05 01:58:14 -0400

    css bootstrapped and set for mobile, icons added, links on sidebar now go to 3 modal windows with email form, how to use, about us

--- src/App.js
+++ src/App.js
@@ -98,6 +98,7 @@ function App() {
 
   const fuse = new Fuse(posts, {
     keys: [
+      'name',
       'body',
       'tags',
     ],
@@ -236,21 +237,23 @@ function App() {
 
 
   {/* ======================== CREATE/POST DIV AND LOGO2 =================================== */}
-  <div id='createDiv' class="container-fluid">
-    <div class="row">  
-      <div class="col-xs-6 mx-auto, col-sm-8 mx-auto, col-md-8 mx-auto">
-        <img id='sideLogo' src='https://i.imgur.com/LUF3DVe.png?1'/>
+  <div className='container'>
+    <div id='createDiv' class="container-fluid">
+      <div class="row">  
+        <div class="col-xs-8 mx-auto, col-sm-8 mx-auto, col-md-10 mx-auto">
+          <img id='sideLogo' src='https://i.imgur.com/LUF3DVe.png?1'/>
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-6 mx-auto, col-sm-8 mx-auto, col-md-8 mx-auto">
-        <form onSubmit={handleFormSubmit}>
-          <input type='text' class="form-control" placeholder='Name' onChange={handleNewName} required/>
-          <input type='text' class="form-control" placeholder='Body' onChange={handleNewBody} required/>
-          <input type='text' class="form-control" placeholder='Img(url)' onChange={handleNewImg} required/>
-          <input type='text' class="form-control" placeholder='Tag' onChange={handleNewTag} required/>
-          <input type='submit' class="form-control" value='Create Post'/>
-        </form><br/>
+      <div class="row">
+        <div class="col-xs-8 mx-auto, col-sm-8 mx-auto, col-md-10 mx-auto">
+          <form  onSubmit={handleFormSubmit}>
+            <input type='text' class="form-control input-sm" placeholder='Name' onChange={handleNewName} required/>
+            <input type='text' class="form-control input-sm" placeholder='Body' onChange={handleNewBody} required/>
+            <input type='text' class="form-control input-sm" placeholder='Img(url)' onChange={handleNewImg} required/>
+            <input type='text' class="form-control input-sm" placeholder='Tag' onChange={handleNewTag} required/>
+            <input type='submit' id='createPost' class="form-control input-sm" value='Create Post'/>
+          </form><br/>
+        </div>
       </div>
     </div>
   </div>
@@ -330,23 +333,114 @@ function App() {
   {/* ================================ CONTACT/INFO/TEAM DIV ===================================== */}
       <div id='info'>
           <div id='projectTeam'>
-            <a href='x'>Contact Us</a>
-            <a href='x'>How to Use</a>
-            <a href='x'>About Us</a>
+          {/* ======== Contact Us - Modal Link ======= */}
+          <a type="button" class="btn" data-toggle="modal" data-target="#contactUs">
+            <i class="bi bi-envelope"> Contact Us</i>
+          </a>
+          {/* ===========-Contact Us - Modal ========== */}
+          <div class="modal fade" id="contactUs" data-backdrop="false" tabindex="-1" role="dialog" aria-labelledby="contactUsTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Contact Bottle Devs</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                <p class="text-dark">
+                <form action="mailto:Admin@BottleApp@.com"
+                    method="POST"
+                    enctype="multipart/form-data"
+                    name="EmailForm">
+                    Name:<br/>
+                    <input type="text" size="19" name="Contact-Name"/><br/>
+                    Email:<br/>
+                    <input type="email" name="Contact-Email"/><br/>
+                    Message:<br/>
+                    <textarea name="Contact-Message" rows="6" cols='20'></textarea><br/>
+                  </form>
+                </p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* ======== How to Use - Modal Link======= */}
+          <a type="button" class="btn" data-toggle="modal" data-target="#howToUse">
+          <i class="bi bi-question-circle"> How to Use</i>
+          </a>
+          {/* ========= How to Use - Modal ========== */}
+          <div class="modal fade" id="howToUse" data-backdrop="false" tabindex="-1" role="dialog" aria-labelledby="howToUseTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">How to use</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                <p class="text-muted">
+                Use Bottle to drop a message with: your name, your message, an img and a tag and connect with friends or others with comments and messages. You can hide and show the comments, the delete button and the edit form with the three button at the top of each message.<br/>
+                Find friends or specific posts easily with fuzzy search at the top of the sidebar. <br/>
+                Chat with friends in Bottle Chat a live chat app that stays on your sidebar while you scrolls friends recent message posts! Enjoy your time in the Bottle
+                </p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">I Get It</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* ========About Us - Modal Link======= */}
+          <a href type="button" class="btn" data-toggle="modal" data-target="#aboutUs">
+          <i class="bi bi-people-fill"> About Us</i>
+          </a>
+          {/* ========== About Us - Modal ======== */}
+          <div class="modal fade" id="aboutUs" data-backdrop="false" tabindex="-1" role="dialog" aria-labelledby="aboutUsTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" font-color="#000000" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">About Us</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body" >
+                <p class="text-dark">
+                We are the team who brought you Bottle! Two Boston based devs...That should be all you need to know...but if you want more our github links are below at the bottom of the sidebar, or you can email us for more info at the contact us link in the sidebar
+                </p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
             <br/>
-            <p>Project Team:<hr/></p>
-            <ul>
-              <li>
-                <a href='https://github.com/Lanny-MacMillan' target="_blank">Lanny</a>
-                <a href='https://github.com/kbrpronet' target="_blank">Kai</a>
-              </li>
-            </ul>
+            <p id='projectTeam'>Project Team:<hr/></p>
+              <a href='https://github.com/Lanny-MacMillan' target="_blank">
+              <i class="bi bi-github"> Lanny</i></a>
+
+              <a href='https://github.com/kbrpronet' target="_blank">
+              <i class="bi bi-github"> Kai</i></a>
           </div>
         </div>
+        <h8 class="tag" >© 2022 The Bottle App.</h8>
     </div>
+    <span>
+        <a href="#top" id="toTop">
+          <i id='toTop' class="bi bi-arrow-up-circle-fill"></i>
+        </a>
+    </span>
     </>
   );
 }
+
 
 export default App;
 
--- src/components/Post.js
+++ src/components/Post.js
@@ -5,19 +5,19 @@ const Post = (props) => {
 <div id='post-div'>
         <div class="container-fluid">
             <div class="row">  
-                <div class="col-4 mx-auto, col-sm-4 mx-auto, col-md-3 mx-auto">
+                <div class="col-10 mx-auto, col-sm-4 mx-auto, col-md-3 mx-auto">
                     <ul class="navbar-nav mx-auto">
-                        <button onClick={() => {props.toggleComments(props.post)}} class="btn btn-secondary btn-sm">Comments</button>
+                        <button onClick={() => {props.toggleComments(props.post)}} id='commentButton' class="btn btn-secondary btn-sm">Comments</button>
                     </ul>
                 </div>
-                <div class="col-4 mx-auto, col-sm-4 mx-auto, col-md-3 mx-auto">
+                <div class="col-10 mx-auto, col-sm-4 mx-auto, col-md-3 mx-auto">
                     <ul class="navbar-nav mx-auto">
-                        <button onClick={() => {props.toggleDelete(props.post)}} class="btn btn-danger btn-sm">Delete</button>
+                        <button onClick={() => {props.toggleDelete(props.post)}} id='deleteButton' class="btn btn-danger btn-sm">Delete</button>
                     </ul>
                 </div>
-                <div class="col-4 mx-auto, col-sm-4 mx-auto, col-md-3 mx-auto">
+                <div class="col-10 mx-auto, col-sm-4 mx-auto, col-md-3 mx-auto">
                         <ul class="navbar-nav mx-auto">
-                        <button onClick={() => {props.toggleEdit(props.post)}} class="btn btn-secondary btn-sm">Edit</button>
+                        <button onClick={() => {props.toggleEdit(props.post)}} id='editButton' class="btn btn-secondary btn-sm">Edit</button>
                     </ul>
                 </div>
             </div>
@@ -25,10 +25,10 @@ const Post = (props) => {
             <br/>
         <div class="container-fluid">    
             <div class="row">  
-                <div class="col-4 mx-auto, col-sm-4 mx-auto, col-md-6 mx-auto">
+                <div id='name' class="col-4 mx-auto, col-sm-4 mx-auto, col-md-6 mx-auto">
                 Name : {props.post.name}
                 </div>
-                <div class="col-4 mx-auto, col-sm-4 mx-auto, col-md-6 mx-auto">
+                <div id='tags' class="col-4 mx-auto, col-sm-4 mx-auto, col-md-6 mx-auto">
                 Tag(s): {props.post.tags}
                 </div>
             </div>
@@ -36,11 +36,11 @@ const Post = (props) => {
     <br/>
     <div class="container-fluid">    
         <div class="row">  
-            <div class="col-4 mx-auto, col-sm-4 mx-auto, col-md-10 mx-auto">
+            <div class="col-10 mx-auto, col-sm-10 mx-auto, col-md-10 mx-auto">
                 <h6>{props.post.body}</h6>
             </div>
             <br/>
-            <div class="col-8 mx-auto, col-sm-10 mx-auto, col-md8 mx-auto">
+            <div class="col-10 mx-auto, col-sm-10 mx-auto, col-md-8 mx-auto">
                 <img src={props.post.img} class="img-fluid" alt="Responsive image"></img>
             </div>
         </div>
@@ -50,7 +50,7 @@ const Post = (props) => {
 {/* ============================================ EDIT POST =========================================*/}
     <div class="container-fluid">    
             <div class="row">  
-                <div class="col-4 mx-auto, col-sm-4 mx-auto, col-md-5 mx-auto">
+                <div class="col-8 mx-auto, col-sm-8 mx-auto, col-md-5 mx-auto">
                     {props.post.showEdit ? 
                     <div class="form-group">
                     <br/>
@@ -68,21 +68,22 @@ const Post = (props) => {
             <div class="row">   
                 <div class="col-8 mx-auto, col-sm-10 mx-auto, col-md8 mx-auto">
                     {props.post.showDelete ? 
-                        <button onClick={(event) => {props.removePost(props.post)}}>Delete...Are you sure?</button>
+                        <button class="btn btn-danger btn-sm" onClick={(event) => {props.removePost(props.post)}}>Delete</button>              
                     : null}
                 </div>
             </div>
         </div>
+
 {/* ========================================== COMMENT POST ========================================= */}
     <div class="container-fluid">    
             <div class="row">  
-                <div class="col-4 mx-auto, col-sm-4 mx-auto, col-md-10 mx-auto">
+                <div class="col-10 mx-auto, col-sm-10 mx-auto, col-md-12 mx-auto">
                     {props.post.showComments ? <h2>Comments</h2> : null}
                     {props.post.showComments ? <hr/> : null}
                 </div>
             </div>
             <div class="row"> 
-                <div id='showComments' class="col-8 mx-auto, col-sm-10 mx-auto, col-md8 mx-auto">
+                <div id='showComments' class="col-10 mx-auto, col-sm-10 mx-auto, col-md-10 mx-auto">
                     {props.post.showComments ? 
                         <>   
                         {props.post.comments.map((comment)=>{
@@ -97,10 +98,10 @@ const Post = (props) => {
 {/* ======================================= NEW COMMENT FORM ======================================= */}
     <div class="container-fluid">    
         <div class="row">  
-            <div class="col-4 mx-auto, col-sm-4 mx-auto, col-md-8 mx-auto">
+            <div class="col-8 mx-auto, col-sm-8 mx-auto, col-md-8 mx-auto">
                 <form onSubmit={(event) => {props.addComment(event, props.post)}}>
                     <input placeholder="What's on your mind..." onChange={props.handleNewComment}  class="form-control" type="text" required/><br/>
-                    <input class="form-control" id='button' type="submit" value="Add Comment"/>
+                    <input id='editSubmit' class="form-control" type="submit" value="Add Comment"/>
                 </form>
             </div>
         </div>
