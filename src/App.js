a46b45c07e2aaa03c5db77b010b48490bf6486c2
Lanny Mac <Lanny@MacBook-Pro.local>
2022-06-05 22:20:19 -0400

    testing chat for heroku

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { Switch } from 'antd';
 
 // const socket = io.connect("http://localhost:3001");
 const socket = io.connect("https://stark-crag-15310-backend.herokuapp.com/");
+// const socket = io.connect("https://mighty-tor-36415-frontend.herokuapp.com/");
 
 function App() {
   const [name, setName] = useState('')
