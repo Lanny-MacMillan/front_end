ec296e4e8f89091c5c9f5249d03e0588f033254e
kbrpronet <kaibrown2889@gmail.com>
2022-06-05 21:48:34 -0400

    resolving merge problems

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,17 +5,17 @@ const Post = (props) => {
 <div id='post-div'>
         <div class="container-fluid">
             <div class="row">  
-                <div class="col-10 mx-auto, col-sm-4 mx-auto, col-md-4 mx-auto">
+                <div class="col-10 mx-auto, col-sm-4 mx-auto, col-md-3 mx-auto">
                     <ul class="navbar-nav mx-auto">
                         <button onClick={() => {props.toggleComments(props.post)}} id='commentButton' class="btn btn-secondary btn-sm">Comments</button>
                     </ul>
                 </div>
-                <div class="col-10 mx-auto, col-sm-4 mx-auto, col-md-4 mx-auto">
+                <div class="col-10 mx-auto, col-sm-4 mx-auto, col-md-3 mx-auto">
                     <ul class="navbar-nav mx-auto">
                         <button onClick={() => {props.toggleDelete(props.post)}} id='deleteButton' class="btn btn-danger btn-sm">Delete</button>
                     </ul>
                 </div>
-                <div class="col-10 mx-auto, col-sm-4 mx-auto, col-md-4 mx-auto">
+                <div class="col-10 mx-auto, col-sm-4 mx-auto, col-md-3 mx-auto">
                         <ul class="navbar-nav mx-auto">
                         <button onClick={() => {props.toggleEdit(props.post)}} id='editButton' class="btn btn-secondary btn-sm">Edit</button>
                     </ul>
@@ -77,9 +77,9 @@ const Post = (props) => {
 {/* ========================================== COMMENT POST ========================================= */}
     <div class="container-fluid">    
             <div class="row">  
-                <div class="col-10 mx-auto, col-sm-10 mx-auto, col-md-12 mx-auto">
-                    {props.post.showComments ? <h2>Comments</h2> : null}
+                <div class="col-10 mx-auto, col-sm-10 mx-auto, col-md-12 mx-auto">      
                     {props.post.showComments ? <hr/> : null}
+                    {props.post.showComments ? <h2>Comments</h2> : null}
                 </div>
             </div>
             <div class="row"> 
